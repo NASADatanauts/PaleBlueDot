ba7f3ac390356361614d4017a31337ec95050a29
Eva Szadeczky-Kardoss <eva@napszel.com>
2017-07-07 17:29:58 +0200

    Thumbnails are only highlighted when hovered. Only clicking selects them.

--- javascript.js
+++ javascript.js
@@ -3,10 +3,21 @@ function getEarths(moment) {
   return $.getJSON("https://epic.gsfc.nasa.gov/api/natural/date/" + moment.format("YYYY-MM-DD") + "?api_key=ecRFCeUylG8hbW4edbzI6GQVu34xTYGfWWvlKOoo");
 }
 
-// string -> <img> html tag
+// string -> string (url of image)
 function getThumbnail(imageName) {
-  var url = 'https://epic.gsfc.nasa.gov/epic-archive/jpg/' + imageName + '.jpg';
-  return $('<img>',{src: url, onmouseover: 'changeImage($(this))'});
+  return 'https://epic.gsfc.nasa.gov/epic-archive/jpg/' + imageName + '.jpg';
+}
+
+// string -> img html tag
+function getTopThumbnailImg(imageName) {
+  var url = getThumbnail(imageName);
+  return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onclick: 'loadHistory($(this))', onmouseout: 'removeHighlight($(this))'});
+}
+
+// string -> img html tag
+function getLeftThumbnailImg(imageName) {
+  var url = getThumbnail(imageName);
+  return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onmouseout: 'removeHighlight($(this))'});
 }
 
 // string -> prints string in dateLabel
@@ -64,13 +75,26 @@ function getBestEarth(earths) {
   return best_earth;
 }
 
-// object -> void, but adds 'selectedTumbnail' class to object
-// This function has to be called with a jquery object
-function changeImage(thumbnail_object) {
-  $("#targetImage").attr("src", thumbnail_object.attr("src"));
-  displayDateFromThumbnail(thumbnail_object.attr("src"));
+// object -> void, but adds 'selectedThumbnail' class to object
+function loadHistory(thumbnail_object) {
   $(".selectedThumbnail").removeClass("selectedThumbnail");
   thumbnail_object.addClass('selectedThumbnail');
+  console.log("Load history to left side");
+}
+
+// object -> void, but removes 'highlightedThumbnail' class from object
+function removeHighlight(thumbnail_object) {
+  thumbnail_object.removeClass('highlightedThumbnail');
+}
+
+// object -> void, but adds 'highlightedThumbnail' class to object
+// and changes main image
+// This function has to be called with a jquery object
+function highlightAndChangeImage(thumbnail_object) {
+  $("#targetImage").attr("src", thumbnail_object.attr("src"));
+  displayDateFromThumbnail(thumbnail_object.attr("src"));
+  $(".highlightedThumbnail").removeClass("highlightedThumbnail");
+  thumbnail_object.addClass('highlightedThumbnail');
 }
 
 // Thumbnails for left side: Europe now and for previous days
@@ -78,14 +102,14 @@ getEarthsesFromNow(10).then(function(earthses) {
   var best_earths = $.map(earthses, getBestEarth);
   best_earths.filter(function (x) { return x != null; });
   var images = $.map(best_earths, function (x) { return x.image; });
-  var thumbnails = $.map(images, getThumbnail);
+  var thumbnails = $.map(images, getLeftThumbnailImg);
   $("#leftThumbnailContainer").prepend(thumbnails);
-  if (thumbnails.length > 0) changeImage(thumbnails[0]);
+  if (thumbnails.length > 0) highlightAndChangeImage(thumbnails[0]);
 });
 
 // Thumbnails for top: lates Earth images from every direction
 getEarthsLatest().then(function(earths) {
   var images = $.map(earths, function (x) { return x.image; });
-  var thumbnails = $.map(images, getThumbnail);
+  var thumbnails = $.map(images, getTopThumbnailImg);
   $("#topThumbnailContainer").prepend(thumbnails);
 });
