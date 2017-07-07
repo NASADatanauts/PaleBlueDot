d472d77c1497f8c66c5f621da2061c3ce86df08c
Eva Szadeczky-Kardoss <eva@napszel.com>
2017-07-07 19:35:22 +0200

    Refactoring: images know their date

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-// moment -> Future([earth])
+// moment -> Future({ e: [earth], d: date })
 function getEarths(moment) {
-  return $.getJSON("https://epic.gsfc.nasa.gov/api/natural/date/" + moment.format("YYYY-MM-DD") + "?api_key=ecRFCeUylG8hbW4edbzI6GQVu34xTYGfWWvlKOoo");
+  return $.getJSON("https://epic.gsfc.nasa.gov/api/natural/date/" + moment.format("YYYY-MM-DD") + "?api_key=ecRFCeUylG8hbW4edbzI6GQVu34xTYGfWWvlKOoo").then(
+    function (ret) {
+      return { e: ret, d: moment.format("YYYY MMMM DD") };
+    });
 }
 
 // string -> string (url of image)
@@ -9,15 +12,31 @@ function getThumbnail(imageName) {
 }
 
 // string -> img html tag
-function getTopThumbnailImg(imageName) {
-  var url = getThumbnail(imageName);
-  return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onclick: 'loadHistory($(this))', onmouseout: 'removeHighlight($(this))'});
+function getTopThumbnailImg(date) {
+  return function (imageName) {
+    var url = getThumbnail(imageName);
+    return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onclick: 'loadHistory($(this))', onmouseout: 'removeHighlight($(this))',
+		      data: {
+			type: 'top',
+			date: date
+		      }
+		     }
+	    );
+  };
 }
 
 // string -> img html tag
-function getLeftThumbnailImg(imageName) {
+function getLeftThumbnailImg(imageName_with_date) {
+  var imageName = imageName_with_date['e'];
+  var date = imageName_with_date['d'];
   var url = getThumbnail(imageName);
-  return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onmouseout: 'removeHighlight($(this))'});
+  return $('<img>',{src: url, onmouseover: 'highlightAndChangeImage($(this))', onmouseout: 'removeHighlight($(this))',
+		    data: {
+		      type: 'left',
+		      date: date
+		      // longitude: longitude
+		    }
+		   });
 }
 
 // string -> prints string in dateLabel
@@ -32,12 +51,12 @@ function displayDateFromThumbnail(source) {
 }
 
 // Returns Earth images for the latest day that has some.
-// () -> Future([earth])
+// () -> Future({e: [earth], d: date })
 function getEarthsLatest() {
   var cont = function(now) {
-    return getEarths(now).then(function (data) {
-      if (data.length != 0) {
-	return Promise.resolve(data);
+    return getEarths(now).then(function (data_and_date) {
+      if (data_and_date['e'].length != 0) {
+	return Promise.resolve(data_and_date);
       } else {
 	return cont(now.subtract(1, 'days'));
       }
@@ -47,12 +66,12 @@ function getEarthsLatest() {
   return cont(moment());
 }
 
-// [moment] -> Future([[earth]])
+// [moment] -> Future({ e: [earth], d: date })
 function getEarthses(moments) {
   return Promise.all($.map(moments, getEarths));
 }
 
-// int -> Future([[earth]])
+// int -> Future([[earth], date])
 function getEarthsesFromNow(how_many) {
   var moments = []
   for (var i = 0; i < how_many ; i++) {
@@ -61,18 +80,23 @@ function getEarthsesFromNow(how_many) {
   return getEarthses(moments);
 }
 
-// [earth] -> earth_or_null (that shows Europe)
-function getBestEarth(earths) {
-  var best_earth = null;
-  $.each(earths, function (_index, earth) {
-    var lon = earth.centroid_coordinates.lon;
-    if (lon > 13 && lon < 25 && (best_earth == null || Math.abs(lon - 19) < best_earth.centroid_coordinates.lon)) {
-      best_earth = earth;
-    }
-  });
-  if (best_earth == null) console.log("no best earth");
-  else console.log("Closest found with name: '" + best_earth.image + "' with value " + best_earth.centroid_coordinates.lon);
-  return best_earth;
+// [earth] -> earth_or_null (that shows earth turned closest to given longitude)
+// getBestEarth :: longitude -> ([[earth], date] -> earth)
+function getBestEarth(longitude) {
+  return function (earths_and_date) {
+    var earths = earths_and_date['e'];
+    var date = earths_and_date['d'];
+    var best_earth = null;
+    $.each(earths, function (_index, earth) {
+      var lon = earth.centroid_coordinates.lon;
+      if (lon > (longitude - 7) && lon < (longitude + 7) && (best_earth == null || Math.abs(lon - longitude) < best_earth.centroid_coordinates.lon)) {
+	best_earth = earth;
+      }
+    });
+    if (best_earth == null) console.log("no best earth");
+    else console.log("Closest found with name: '" + best_earth.image + "' with value " + best_earth.centroid_coordinates.lon);
+    return { e: best_earth, d: date };
+  };
 }
 
 // object -> void, but adds 'selectedThumbnail' class to object
@@ -91,6 +115,7 @@ function removeHighlight(thumbnail_object) {
 // and changes main image
 // This function has to be called with a jquery object
 function highlightAndChangeImage(thumbnail_object) {
+  // console.log(thumbnail_object.data());
   $("#targetImage").attr("src", thumbnail_object.attr("src"));
   displayDateFromThumbnail(thumbnail_object.attr("src"));
   $(".highlightedThumbnail").removeClass("highlightedThumbnail");
@@ -98,18 +123,20 @@ function highlightAndChangeImage(thumbnail_object) {
 }
 
 // Thumbnails for left side: Europe now and for previous days
-getEarthsesFromNow(20).then(function(earthses) {
-  var best_earths = $.map(earthses, getBestEarth);
-  best_earths.filter(function (x) { return x != null; });
-  var images = $.map(best_earths, function (x) { return x.image; });
-  var thumbnails = $.map(images, getLeftThumbnailImg);
+getEarthsesFromNow(20).then(function(earthses_and_dates) {
+  var best_earths_with_dates = $.map(earthses_and_dates, getBestEarth(-80));
+  best_earths_with_dates = best_earths_with_dates.filter(function (x) { return x['e'] != null; });
+  var images_with_dates = $.map(best_earths_with_dates, function (x) { return { e: x['e'].image, d: x['d'] }; });
+  var thumbnails = $.map(images_with_dates, getLeftThumbnailImg);
   $("#leftThumbnailContainer").prepend(thumbnails);
   if (thumbnails.length > 0) highlightAndChangeImage(thumbnails[0]);
 });
 
-// Thumbnails for top: lates Earth images from every direction
-getEarthsLatest().then(function(earths) {
+// Thumbnails for top: latest Earth images from every direction
+getEarthsLatest().then(function(earths_and_date) {
+  var earths = earths_and_date['e'];
+  var date = earths_and_date['d'];
   var images = $.map(earths, function (x) { return x.image; });
-  var thumbnails = $.map(images, getTopThumbnailImg);
+  var thumbnails = $.map(images, getTopThumbnailImg(date));
   $("#topThumbnailContainer").prepend(thumbnails);
 });
