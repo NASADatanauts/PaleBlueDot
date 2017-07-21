dde66b7defaef682b652f651f84222c9d337bc8d
Eva Szadeczky-Kardoss <eva@napszel.com>
2017-07-21 20:27:37 +0200

    Huge refactoring. MUCH improved pre-caching of images.

--- javascript.js
+++ javascript.js
@@ -1,136 +1,171 @@
-var currentDateIndex;
-var currentLongitude = 19; // Europe
-var alreadyCachedDateIndex = nasaarray.length;
+// Invariants:
+//   - selectedColumn is the index of the closest earth rotation according to goalLongitude in selectedRow
+//   - on the UI the date shown is always nasaarray[selectedRow].d
+//   - on the UI the shown image is always the one pointed by selectedRow,selectedColumn
 
-function preloadImages(dateIndex) {
-  var array = getAllImageUrls(nasaarray[dateIndex].d, nasaarray[dateIndex].i);
-  if (!preloadImages.list) {
-    preloadImages.list = [];
-  }
-  var list = preloadImages.list;
-  for (var i = 0; i < array.length; i++) {
+// index in nasaarray[____]
+var selectedRow = nasaarray.length - 1;
+// index in nasaarray[selectedrow].{i,l}[____]
+// initialized in constructor
+var selectedColumn;
+// goal of the user, starting value is Europe
+var goalLongitude = 19;
+
+// class Preload
+var preloadedImages = {}; // don't let Chrome cancel the images!
+var inFlightImages = 0;
+function preloadImage(row, col) {
+  var url = getImageURL(row, col);
+  if (!preloadedImages[url]) {
     var img = new Image();
     img.onload = function() {
-      var index = list.indexOf(this);
-      if (index !== -1) {
-        // remove image from the array once it's loaded for memory consumption reasons
-        list.splice(index, 1);
-      }
+      preloadedImages[url] = undefined;
+      inFlightImages--;
     }
-    list.push(img);
-    img.src = array[i];
-  }
-  if (dateIndex < alreadyCachedDateIndex) {
-    alreadyCachedDateIndex = dateIndex;
-  }
-}
-
-function preloadImagesForPreviousDays(howMany) {
-  for (var i = currentDateIndex - 1; i > currentDateIndex - howMany - 1; i--) {
-    preloadImages(i);
+    img.onabort = function() {
+      inFlightImages--;
+    }
+    img.onerror = img.onabort;
+    inFlightImages++;
+    img.src = url;
+    preloadedImages[url] = img;
   }
 }
 
-// date, array of strings -> array of image urls
-function getAllImageUrls(date, imageNames) {
-  var ret = [];
-  $.each(imageNames, function (index, image) {
-    ret.push(getImageUrl(date, image));
-  });
-  return ret;
+function preloadCancelImage(row, col) {
+  var url = getImageURL(row, col);
+  var pre = preloadedImages[url];
+  if (pre) {
+    pre.src = "http://placekitten.com/200/300";
+    preloadedImages[url] = undefined;
+  }
 }
 
-// date, string -> string (url of image)
-function getImageUrl(date, imageName) {
-  var mdate = moment(date, "YYYY-MM-DD");
+setInterval(function () {
+  if (inFlightImages > 0) {
+    $("#loading").removeClass("hidden");
+    $("#loading").text("Loading... (" + inFlightImages + ")");
+  } else {
+    $("#loading").addClass("hidden");
+  }
+}, 200);
+// end of class
+
+function getImageURL(row, col) {
+  var mdate = moment(nasaarray[row].d, "YYYY-MM-DD");
+  var imageName = nasaarray[row].i[col];
   return 'https://epic.gsfc.nasa.gov/archive/natural/'
     + mdate.format('YYYY') + '/' + mdate.format('MM') + '/' + mdate.format('DD')
-    + '/jpg/' + imageName + '.jpg?api_key=ecRFCeUylG8hbW4edbzI6GQVu34xTYGfWWvlKOoo';
+    + '/jpg/' + imageName + '.jpg';
 }
 
-// longitude and array of nasa Objects from ONE day -> index of element that shows earth closest to given longitude
-function getBestEarth(longitude, imagesOfADay) {
-  var longitudes = imagesOfADay.l;
+function getSelectedImageURL() {
+  return getImageURL(selectedRow, selectedColumn);
+}
+
+function preloadImagesForRow(row) {
+  for (var col = 0; col < nasaarray[row].n; col++) preloadImage(row, col);
+}
+
+function preloadCancelForRow(row) {
+  for (var col = 0; col < nasaarray[row].n; col++) preloadCancelImage(row, col);
+}
+
+function preloadCancelForSelectedRow() {
+  preloadCancelForRow(selectedRow);
+}
+
+// around a row
+function preloadImagesForColumn(row) {
+  for (var i=-5; i<=5; i++) {
+    var currentRow = row + i;
+    if (currentRow == row || currentRow >= nasaarray.length || currentRow < 0)
+      continue;
+
+    preloadImage(currentRow, getColumnFromLongitude(currentRow));
+  }
+}
+
+var timerPreloadImagesForSelectedPoint = null;
+function preloadImagesForSelectedPoint() {
+  if (timerPreloadImagesForSelectedPoint) {
+    clearTimeout(timerPreloadImagesForSelectedPoint);
+    timerPreloadImagesForSelectedPoint = null;
+  }
+
+  timerPreloadImagesForSelectedPoint = setTimeout(function () {
+    preloadImagesForRow(selectedRow);
+    preloadImagesForColumn(selectedRow);
+  }, 400);
+}
+
+// given a row index, gives us the best column index in that row according to goalLongitude
+function getColumnFromLongitude(row) {
+  // TODO: map the longitudes with Math.abs(... - ...) and then simply choose the minIndex of the array
+  var longitudes = nasaarray[row].l;
   var best_earth = 0;
+  var best_value = Math.abs(longitudes[best_earth] - goalLongitude)
   $.each(longitudes, function (index, earth) {
-    if (Math.abs(longitudes[index] - longitude) < Math.abs(longitudes[best_earth] - longitude)) {
+    if (Math.abs(longitudes[index] - goalLongitude) < best_value) {
       best_earth = index;
+      best_value = Math.abs(longitudes[index] - goalLongitude);
     }
   });
-  if (Math.abs(longitudes[best_earth] - longitude) > 9) {
-    console.warn("Even best image found is way off. Requested closest to %i and found %i.", longitude, longitudes[best_earth]);
+  if (Math.abs(longitudes[best_earth] - goalLongitude) > 9) {
+    console.warn("Even best image found is way off. Requested closest to %i and found %i.", goalLongitude, longitudes[best_earth]);
   }
   return best_earth;
 }
-  
-function changeDateOnUI(newDate) {
-  $("#dateLabel").text(moment(newDate).format("YYYY MMMM DD"));
+
+function setColumnFromLongitude() {
+  selectedColumn = getColumnFromLongitude(selectedRow);
 }
 
-// shows the closest earth rotation to previous on given date
-function gotoDate(dateIndex) {
-  var currentDate = nasaarray[dateIndex].d;
-  changeDateOnUI(currentDate);
-  
-  var earthRotation = getBestEarth(currentLongitude, nasaarray[dateIndex]);
-  $("#targetImage").attr("src", getImageUrl(currentDate, nasaarray[dateIndex].i[earthRotation]));
-}
+function activateSelectedRow() {
+  var selectedDate = nasaarray[selectedRow].d;
+  $("#dateLabel").text(moment(selectedDate).format("YYYY MMMM DD"));
 
-// rotates earth to index within the day that is currently selected
-function rotateEarth(lonIndex) {
-  var currentDate = nasaarray[currentDateIndex].d;
-  changeDateOnUI(currentDate); // this shouldn't be needed, just here for debug
+  setColumnFromLongitude();
+  $("#targetImage").attr("src", getSelectedImageURL());
 
-  currentLongitude = nasaarray[currentDateIndex].l[lonIndex];
-  $("#targetImage").attr("src", getImageUrl(currentDate, nasaarray[currentDateIndex].i[lonIndex]));
+  preloadImagesForSelectedPoint();
 }
 
 function rotateEarthWithMouseMove(event) {
   mouseX = event.pageX;
 
-  var calibration = $(window).width() / nasaarray[currentDateIndex].n;
-  var imageX = Math.floor(mouseX / calibration);
+  var columnWidth = $(window).width() / nasaarray[selectedRow].n;
+  selectedColumn = Math.floor(mouseX / columnWidth);
+  goalLongitude = nasaarray[selectedRow].l[selectedColumn];
 
-  rotateEarth(imageX);
+  $("#targetImage").attr("src", getSelectedImageURL());
+
+  preloadImagesForSelectedPoint();
 }
 
-function gotoDateWithScroll(event) {
+function selectRowWithScroll(event) {
   if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
     // scoll up
-    if (currentDateIndex < nasaarray.length - 1) {
-      currentDateIndex = currentDateIndex + 1;
-      gotoDate(currentDateIndex);
+    if (selectedRow < nasaarray.length - 1) {
+      preloadCancelForSelectedRow();
+      selectedRow = selectedRow + 1;
+      activateSelectedRow();
     }
   } else {
     // scroll down
-    if (currentDateIndex > 0) {
-      currentDateIndex = currentDateIndex - 1;
-      gotoDate(currentDateIndex);
- //     if (currentDateIndex - alreadyCachedDateIndex == 2) {
-//	preloadImagesForPreviousDays(10);
- //     }
+    if (selectedRow > 0) {
+      preloadCancelForSelectedRow();
+      selectedRow = selectedRow - 1;
+      activateSelectedRow();
     }
   }
 }
 
 $(document).ready(function () {
-  // Find the most recent day that has images
-  var day = nasaarray.length - 1;
-  while (nasaarray[day].length == 0) {
-    day = day - 1;
-  }
-  currentDateIndex = day;
-  
-  // load that day's image (rotation is currentLongitude)
-  gotoDate(currentDateIndex);
-
-  // pre-load all images for that day
-  preloadImages(currentDateIndex);
-
-  // start pre-loading all images for previous 3 days (from currentDateIndex)
-  preloadImagesForPreviousDays(3);
+  // load selectedRow's image (lates day with images and rotation goalLongitude)
+  activateSelectedRow();
   
   $(window).mousemove(rotateEarthWithMouseMove);
 
-  $(window).bind('mousewheel DOMMouseScroll', gotoDateWithScroll);
+  $(window).bind('mousewheel DOMMouseScroll', selectRowWithScroll);
 });
